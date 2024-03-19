7e8d57bb76b1a0201005a112545eb9313027cae9
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-03-19 20:05:06 +0000

    hasta 8.3 hecho

--- exercises/08.2-Count-On/app.js
+++ exercises/08.2-Count-On/app.js
@@ -1,9 +1,12 @@
-let myArray = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
+let myArray = [42, true, "towel", [2, 1], 'hello', 34.4, { "name": "juan" }];
 
 let hello = [];
-for(let i = 0; i < myArray.length; i++) {
-    let item = myArray[i];
+for (let i = 0; i < myArray.length; i++) {
+    let item = typeof (myArray[i]);
     // MAGIC HAPPENS HERE
+    if (item == "object") {
+        hello.push(myArray[i]);
+    }
 }
 
 console.log(hello)
--- exercises/08.3-Sum-all-items/app.js
+++ exercises/08.3-Sum-all-items/app.js
@@ -1,6 +1,10 @@
 function sumTheElements(theArray) {
 	let total = 0;
 	// Your code here
-
+	for (let i = 0; i < theArray.length; i++) {
+		total = total + theArray[i];
+	}
 	return total;
 }
+
+console.log(sumTheElements([1,2,3,4,5,6]))
