7b5a3889ab088828b057c223f59ee3f2804b87c7
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-03-20 10:35:06 +0000

    hasta 14

--- exercises/12-Delete-element/app.js
+++ exercises/12-Delete-element/app.js
@@ -1,7 +1,13 @@
-let people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emilio'];
+let people = ['juan', 'ana', 'michelle', 'daniella', 'stefany', 'lucy', 'barak', 'emilio'];
 
 // Your code below
+let newArr = [];
+function deletePerson(nombre) {
 
+    newArr = people.filter((elemento) => elemento != nombre);
+
+    return newArr;
+}
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));
 console.log(deletePerson('emilio'));
--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,3 +1,21 @@
 let listOfNumbers = [4, 80, 85, 59, 37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68];
 
 // Your code here
+function mergeTwoList(arr1) {
+    let odd = [];
+    let even = [];
+    let oddEven = [];
+    arr1.forEach(element => {
+        if (element % 2 === 0) {
+            even.push(element);
+        } else if (element % 2 != 0) {
+            odd.push(element);
+        }
+    });
+
+    console.log(odd);
+    console.log(even);
+
+    return oddEven = odd.concat(even);
+}
+mergeTwoList([5, 84, 2, 65, 94, 32, 85, 1, 29, 6, 4, 7, 13, 14, 95]);
