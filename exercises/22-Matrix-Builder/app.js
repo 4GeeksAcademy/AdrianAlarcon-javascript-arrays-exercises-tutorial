2eb4195eb515a386515270ce295207a63e08180b
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-03-21 10:49:23 +0000

    a

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,5 +1,14 @@
 // Your code here
-
+function matrixBuilder(numero) {
+    let matrix = [];
+    for (let i = 0; i < numero; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < numero; j++) {
+            matrix[i][j] = Math.round(Math.random());
+        }
+    }
+    return matrix;
+}
 
 
 // Do not change anything from this line down
--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,9 +1,24 @@
 // Your code here
-
+function lyricsGenerator(myArray) {
+    let frase = "";
+    let countOne = 0;
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] == 0) {
+            frase += "Boom ";
+            countOne = 0;
+        } else if (myArray[i] == 1) {
+            frase += "Drop the bass ";
+            countOne += 1;
+        } if (countOne === 3) {
+            frase += "!!!Break the bass!!! "
+        }
+    }
+    return frase;
+}
 
 // Don't change anything below this line
-console.log(lyricsGenerator([0,0,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,0]))
-console.log(lyricsGenerator([1,0,1]))
-console.log(lyricsGenerator([1,1,1]))
+console.log(lyricsGenerator([0, 0, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 1, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 0]))
+console.log(lyricsGenerator([1, 0, 1]))
+console.log(lyricsGenerator([1, 1, 1]))
